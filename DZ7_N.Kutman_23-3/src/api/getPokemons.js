d1e7c764fe326fe841abecae079f8edb4ce72f28
Aesthetic <117440607+Scarbete@users.noreply.github.com>
2023-05-07 23:05:29 +0600

    DZ7_N.Kutman_23-3

---
+++ DZ7_N.Kutman_23-3/src/api/getPokemons.js
@@ -0,0 +1,25 @@
+import { requester } from '../requester'
+
+export const fetchPoke = async ({ limit, offset }) => {
+    try {
+        const { data } = await requester.get(`/pokemon?limit=${limit}&offset=${offset}`)
+        return data
+    }
+    catch (error) {console.log(error)}
+}
+
+// export const getPokemonName = async ( name ) => {
+//     try {
+//         const { data } = await requester.get(`/pokemon/${name}`)
+//         return data
+//     }
+//     catch (error) {console.log(error)}
+// }
+
+export const getPokemonId = async ( id ) => {
+    try {
+        const { data } = await requester.get(`/pokemon/${id}`)
+        return data
+    }
+    catch (error) {console.log(error)}
+}
